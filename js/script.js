262b0fd135140ee6b64028f1db7c8f89fe055237
Ali82Aps <a.82aps@gmail.com>
2024-07-03 19:23:07 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const swiperSlider = new Swiper('.swiper', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination'
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+
+
+})
+
